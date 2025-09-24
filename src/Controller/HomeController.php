6926f57b81de2b0cefa149e14583740560200358
FatoumataDIABATE <diabatefatoumata06@icloud.com>
2025-09-24 15:43:39 +0200

    Mon travail en cours
    # Conflicts:
    #	config/packages/security.yaml
    
    # Conflicts:
    #	templates/base.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,35 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(Request $request): Response
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
     {
-        $searchResult = null;
-        $errorMessage = null;
-
-        $pseudo = $request->query->get('pseudo'); // récupération du pseudo depuis la barre de recherche
-
-        if ($pseudo) {
-            $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['pseudo' => $pseudo]);
-
-            if ($user) {
-                $searchResult = $user;
-            } else {
-                $errorMessage = "Utilisateur introuvable.";
-            }
-        }
-
-        return $this->render('home/index.html.twig', [
-            'searchResult' => $searchResult,
-            'errorMessage' => $errorMessage,
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
